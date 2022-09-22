959a32af5195bf93e72cc1fd6fa401cb59063f9b
eduardopaaaa <eddy.nator@hotmail.com>
2022-09-22 14:26:56 -0600

    components add + CSS

--- bugger/src/App.js
+++ bugger/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import {React, useState} from "react";
 import './App.css';
 
 function App() {
+
+    let [showBug, setShowBug] = useState(false)
+    let [showTask, setShowTasks] = useState(true)
+
   return (
     <div className="App">
       <header className="App-header">
-        hello there, general kenobi
+        hello there, general kenobi 
+
+        sfgdsdf
       </header>
     </div>
   );
